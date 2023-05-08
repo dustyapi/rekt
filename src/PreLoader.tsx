68b30cb04de0145af667dda03d97dfe78ca913cf
z <z@pop-os.localdomain>
2023-05-08 23:55:56 +0530

    added bg

--- src/PreLoader.tsx
+++ src/PreLoader.tsx
@@ -1,15 +1,17 @@
 import { preLoaderAnim } from "./animations/index";
 import "./preLoader.css";
 import React, { useEffect } from "react";
+
 const PreLoader = () => {
   useEffect(() => {
     preLoaderAnim();
   }, []);
 
   return (
-    <div className="preloader">
+    <div className="preloader" >
       <div className="texts-container">
         <h1>GET REKT</h1>
+        {/* <img src="./fire.gif"></img> */}
       </div>
     </div>
   );
